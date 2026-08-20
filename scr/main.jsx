d3d0f80f393010ce55467c28bf7fbbd913e0ee4e
tchoudamlaurencenicole-crypto <tchoudamlaurencenicole@gmail.com>
2026-08-20 20:46:23 +0100

    Update main.jsx

--- scr/main.jsx
+++ scr/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PronoApp from "./PronoApp.jsx";
+import PronoApp from "./PronoFoot.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
